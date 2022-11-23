2b02ae70056f9d848decefbe76a2adae880f51b0
Pcauderlier <pierre.cauderlier@outlook.com>
2022-11-23 17:00:01 +1100

    Timer fonctionelle avec un prime le bouton pause !

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,67 @@
+import { useEffect, useRef, useState } from 'react';
+import '../styles/Timer.css'
+
+
 
 function Timer(){
+   
+    
+    const [minute , updateMinute] = useState(0);
+    const [seconde , updateSeconde] = useState(0);
+    const [isTimerOn , updateIsTimerOn] = useState(false)
+    const [isPaused , updateisPaused] = useState(false)
+    
+
+    function timerRun(){
+        if (isTimerOn){
+            if (seconde === 0){
+                if (minute === 0){
+                    return updateIsTimerOn(false)
+                }
+                else{
+                    updateMinute(()=>minute-1)
+                    updateSeconde(59)
+                }
+            }
+            else{
+                updateSeconde(()=>seconde -1)
+            }
+        }
+    }
+    function setTimer(m,s){
+        updateIsTimerOn(true)
+        updateMinute(m);
+        updateSeconde(s)        
+    }
+    function timerPause(){
+        updateisPaused(()=> !isPaused);
+        updateIsTimerOn(()=> !isTimerOn)
+    }
+    useEffect(()=>{
+        let timer = setTimeout(()=>{
+            timerRun()},
+            1000
+        );
+        return ()=> clearTimeout(timer)
+    },
+    [minute,seconde,isPaused]
+    )
+    
+
     return (
-        <div>COUCOU</div>
+        <div className="timer-box">
+            <h1>
+                Test Timer
+            </h1>
+            <div>
+                {minute < 10 ? `0${minute}` : minute.toString()} : 
+                {seconde < 10 ? `0${seconde}` : seconde.toString()}
+            </div>
+            <button onClick={()=> setTimer(1,30) }>Lancer 01:30 </button>
+            <button onClick={()=> timerPause() }>Pause</button>
+
+        </div>
     )
 }
 export default Timer
+
